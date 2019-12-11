b8a4c6de115805d11560863de4f7a3a9cd21eba7
Stevan Bugarski <bugarski.stevan@gmail.com>
2019-12-10 20:28:12 -0500

    react app created

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+const Component = React.Component;
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello</h1>
+    </div>
+  );
+}
+
+
+export default App;
