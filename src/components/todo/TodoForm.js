41f796c9c50bea89573667e1dd1ccd66a06dc0b2
Stevan Bugarski <bugarski.stevan@gmail.com>
2019-12-21 12:14:58 -0500

    Edit button updated

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -4,15 +4,14 @@ import Todo from './TodoList';
 import TodoInput from './TodoInput';
 const TodoForm = () => {
   const [text, setText] = useState('');
-  const [editItem, setEditItem] = useState(false);
   const [todos, setTodos] = useState([
     'Practise React',
     'Practise Java Script'
   ]);
-
   const handleChange = e => {
     setText(e.target.value);
   };
+
   const handleAdd = e => {
     e.preventDefault();
     const letterNumber =
@@ -22,7 +21,6 @@ const TodoForm = () => {
     if (text.match(letterNumber) && text.length < 30) {
       setTodos(newTodos);
       setText('');
-      setEditItem(false);
     }
   };
   const handleDelete = index => {
@@ -34,14 +32,6 @@ const TodoForm = () => {
     e.preventDefault();
     setText('');
     setTodos([]);
-  };
-  const handleEdit = id => {
-    const items = [...todos];
-    const itemId = items[id];
-    const filteredItems = items.filter(item => item !== itemId);
-    setText(itemId);
-    setTodos(filteredItems);
-    setEditItem(true);
   };
 
   return (
@@ -57,18 +47,17 @@ const TodoForm = () => {
               value={text}
               type='text'
               handleChange={handleChange}
-              handleAdd={handleAdd}
-              editItem={editItem}
+              handleAdd={e => handleAdd(e)}
             />
             <div>
               <h3 className='text-capitalize text-center'>todo list</h3>
               {todos.map((item, index) => (
                 <Todo
                   text={item}
+                  value={text}
                   key={index}
                   index={index}
                   clickHandler={() => handleDelete(index)}
-                  handleEdit={() => handleEdit(index)}
                 />
               ))}
             </div>
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = props => {
-  return (
-    <div className='list-group-item text-capitalize d-flex justify-content-between my-2'>
-      <p>{props.text} </p>
-      <div>
-        <span className='mx-2 text-success' onClick={props.handleEdit}>
-          <i className='fa fa-edit'></i>
+  const [checked, setChecked] = useState(false);
+  const [editItem, setEditItem] = useState(false);
+  const [editItemtext, setEditItemText] = useState('');
+  const handleCheck = () => {
+    setChecked(!checked);
+  };
+  const onChange = e => {
+    setEditItemText(e.target.value);
+  };
+  const handleEditItem = () => {
+    setEditItem(!editItem);
+  };
+  const checkOut = e => {
+    setEditItem(false);
+    console.log('Click from ok button', editItem);
+  };
+  return editItem ? (
+    <div>
+      <input value={editItemtext} onChange={onChange} />
+      <button onClick={handleEditItem}>X</button>
+      <button onClick={checkOut}>Ok</button>
+    </div>
+  ) : (
+    <div>
+      <div className='list-group-item text-capitalize d-flex justify-content-between my-2'>
+        <span>
+          <i
+            data-index={props.index}
+            className={!checked ? 'dot' : 'dotChecked'}
+            value={checked}
+            onClick={handleCheck}
+          ></i>
         </span>
-        <span
-          className='mx-2 text-danger'
-          data-index={props.index}
-          onClick={props.clickHandler}
-        >
-          <i className='fa fa-trash'></i>
-        </span>
+
+        <div>
+          <p className={!checked ? null : 'lineThrough'}>
+            {editItemtext ? editItemtext : props.text}
+          </p>
+        </div>
+        <div>
+          <span className='mx-2 text-success'>
+            <i
+              className='fa fa-edit'
+              data-index={props.index}
+              onClick={handleEditItem}
+            ></i>
+          </span>
+          <span className='mx-2 text-danger' onClick={props.clickHandler}>
+            <i className='fa fa-trash'></i>
+          </span>
+        </div>
       </div>
     </div>
   );
